338f47700388c7078e9ff027137b533adc9afc12
jonasegf <31020032+jonasegf@users.noreply.github.com>
2022-06-09 17:45:25 -0300

    second commit?

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,5 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
-import com.fullcycle.admin.catalogo.application.UseCase;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
