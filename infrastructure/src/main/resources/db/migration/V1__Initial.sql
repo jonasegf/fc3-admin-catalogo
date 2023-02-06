3f1e587160d564d52f948d4a8088fd331886edfd
jonasegf <jonas.edu@hotmail.com>
2023-02-05 21:06:31 -0300

    Category migration

--- infrastructure/src/main/resources/db/migration/V1__Initial.sql
+++ infrastructure/src/main/resources/db/migration/V1__Initial.sql
@@ -1,3 +1,9 @@
-create table category (
- id BINARY(16) primary key
+CREATE TABLE category (
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    description varchar(4000),
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at DATETIME(6) not null,
+    updated_at DATETIME(6) not null,
+    deleted_at DATETIME(6)
 )
